fe2ff61a4cca5b923e930b6337532a25c70746a7
Aboulaye Drabo <drabis47@gmail.com>
2023-03-24 11:34:10 -0400

    created different component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function TodoForm() {
+export default function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    // prevent default action
+    e.preventDefault();
+    // add todo
+    addTodo(value);
+    // clear form after submission
+    setValue("");
+  };
   return (
-    <form className="TodoForm">
+    <form onSubmit={handleSubmit} className="TodoForm">
       <input
         type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         className="todo-input"
         placeholder="What is the task today?"
       />
-      <buuton type="submit" className="todo-btn"> Add Task</buuton>
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
     </form>
   );
 }
